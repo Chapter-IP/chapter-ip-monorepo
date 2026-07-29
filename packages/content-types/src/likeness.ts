94cbeb7798902e48da1c486f3b03f499f4d0d261
Evgen Pluzhnikov <eugene.pluzhnikov@gmail.com>
2026-07-29 14:01:32 +0300

    Display uploaded files in likeness license modal

--- packages/content-types/src/likeness.ts
+++ packages/content-types/src/likeness.ts
@@ -185,6 +185,7 @@ export type LikenessDetails = {
   approveFinalUse: boolean
   images: LikenessImage[]
   media: LikenessMedia[]
+  files: string[]
 }
 
 export type LikenessPurchase = LikenessDetails
