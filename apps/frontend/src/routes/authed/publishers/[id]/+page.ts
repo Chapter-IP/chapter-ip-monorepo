51a68c800acd9cd43bf110967d9a9f70b9f80033
Evgen Pluzhnikov <eugene.pluzhnikov@gmail.com>
2026-09-14 16:15:28 +0300

    Add creative works to publisher page

--- apps/frontend/src/routes/authed/publishers/[id]/+page.ts
+++ apps/frontend/src/routes/authed/publishers/[id]/+page.ts
@@ -2,6 +2,7 @@ import { configStore, ContractName } from '$lib/stores/config.svelte'
 import { getMembershipPrice } from '$lib/membership'
 import { toLikenessItems } from '../../likeness/likeness'
 import { toLocationItems } from '../../location/location'
+import { toWorkItems } from '../../creative-works/works'
 import { STATUS } from '@repo/content-types/content'
 
 export const load = async ({ params, parent }) => {
@@ -20,6 +21,11 @@ export const load = async ({ params, parent }) => {
   const items = [
     ...toLikenessItems(contentItems, contractAddress).map((i) => ({ ...i, type: 'likeness' as const })),
     ...toLocationItems(contentItems, contractAddress).map((i) => ({ ...i, type: 'location' as const })),
+    ...toWorkItems(contentItems, contractAddress).map((w) => ({
+      ...w,
+      name: w.title,
+      type: 'creative-works' as const,
+    })),
   ]
 
   const subscriptionPrice = publisher.evmAddress ? await getMembershipPrice(publisher.evmAddress) : 0
